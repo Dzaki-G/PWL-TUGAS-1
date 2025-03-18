929ba11f17a102193322228a07922c21ab9e2ae9
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 18:56:51 +0700

    Add files via upload

---
+++ Django-ecommerce-microservices/coupons-service/database/coupons_db.sql
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'coupons_db',                     
+        'USER': 'coupons_user',                  
+        'PASSWORD': 'coupons_password',           
+        'HOST': 'localhost',                   
+        'PORT': '3306',                        
+    }
+}
