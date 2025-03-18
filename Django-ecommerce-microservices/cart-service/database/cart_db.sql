4d8fcf6b3f0bb54560e285e4742394e0cb6523c1
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 18:55:37 +0700

    Add files via upload

---
+++ Django-ecommerce-microservices/cart-service/database/cart_db.sql
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'cart_db',                     
+        'USER': 'cart_user',                  
+        'PASSWORD': 'cart_password',           
+        'HOST': 'localhost',                   
+        'PORT': '3306',                        
+    }
+}
