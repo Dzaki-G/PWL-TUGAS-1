29761f23d3c50300b0f52c08a290a94b7175a4ff
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 19:03:42 +0700

    Update manage.py

--- Django-ecommerce-microservices/order-service/manage.py
+++ Django-ecommerce-microservices/order-service/manage.py
@@ -1,7 +1,22 @@
-from django.http import JsonResponse
+#!/usr/bin/env python
+import os
+import sys
 
-def create_order(request):
-    return JsonResponse({"message": "Order created!"})
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'order_service.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        try:
+            import django
+            django.setup()
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+    execute_from_command_line(sys.argv)
 
-def order_status(request):
-    return JsonResponse({"message": "Order status!"})
+if __name__ == '__main__':
+    main()
