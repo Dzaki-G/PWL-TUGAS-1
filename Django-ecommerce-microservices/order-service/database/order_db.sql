860fdad0a2686d7ac67a762f3588aa76490b6aba
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 19:02:20 +0700

    Add files via upload

---
+++ Django-ecommerce-microservices/order-service/database/order_db.sql
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'order_db',                     
+        'USER': 'order_user',                  
+        'PASSWORD': 'order_password',           
+        'HOST': 'localhost',                   
+        'PORT': '3306',                        
+    }
+}
