a01c760cabc0804ceb7aa3fd320e15b5af3e7f36
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 19:01:35 +0700

    Add files via upload

---
+++ Django-ecommerce-microservices/shop-service/database/shop_db.sql
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'shop_db',                     
+        'USER': 'shop_user',                  
+        'PASSWORD': 'shop_password',           
+        'HOST': 'localhost',                   
+        'PORT': '3306',                        
+    }
+}
