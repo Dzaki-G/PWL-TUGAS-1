9a005cc897b241caed786d0a6db4551513ff3f88
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-03-18 19:00:42 +0700

    Update shop_db.sql

--- Django-ecommerce-microservices/shop-service/database/shop_db.sql
+++ Django-ecommerce-microservices/shop-service/database/shop_db.sql
@@ -1,10 +1,1 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'shop_db',                     
-        'USER': 'shop_user',                  
-        'PASSWORD': 'shop_password',           
-        'HOST': 'localhost',                   
-        'PORT': '3306',                        
-    }
-}
+
